64a6caa7fcff09db03fbb481c1b20e145bfed746
xuwei-k <6b656e6a69@gmail.com>
2015-06-03 23:51:43 +0900

    simplify ShrinkExample

--- src/test/scala/scalaprops/examples/ShrinkExamples.scala
+++ src/test/scala/scalaprops/examples/ShrinkExamples.scala
@@ -1,19 +1,12 @@
 package scalaprops
 package examples
 
-import scalaprops._
-import Gen._
-import scalaz._
-import scalaz.std.stream._
-
 // port from https://github.com/rickynils/scalacheck/wiki/User-Guide
 
 object ShrinkExample extends Scalaprops {
 
-  def shrinkTuple2[T1, T2](implicit T1: Shrink[T1], T2: Shrink[T2]): Shrink[(T1, T2)] =
-    Shrink.shrink { case (t1, t2) => Apply[Stream].tuple2(T1(t1), T2(t2)) }
-
-  val origShrink = Property.forall0(Gen.tuple2[Int, Int], shrinkTuple2[Int, Int]) { case (t1, t2) =>
+  val origShrink = Property.property{ (t1: Int, t2: Int) =>
     Property.prop(t1 >= t2)
   }
+
 }
