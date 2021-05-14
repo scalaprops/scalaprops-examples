7664c54f981b960f1220b3f18d1f20e6958db29f
kenji yoshida <6b656e6a69@gmail.com>
2021-05-14 10:18:50 +0900

    add Scala 3

--- src/test/scala/scalaprops/examples/PropertyExample.scala
+++ src/test/scala/scalaprops/examples/PropertyExample.scala
@@ -8,9 +8,9 @@ import Property.{property, implies, prop}
 
 object PropertyExample extends Scalaprops {
 
-  val makeList = property { n: Int =>
+  val makeList = property { (n: Int) =>
     implies(n >= 0 && n < 10000, prop(List.fill(n)("").length == n))
   }
 
-  val trivial = property { n: Int => Bool.bool(n == 0).implies(n == 0) }
+  val trivial = property { (n: Int) => Bool.bool(n == 0).implies(n == 0) }
 }
