ec1f9574713faf070963105956d63b87e8c3ca41
kenji yoshida <6b656e6a69@gmail.com>
2021-05-14 10:18:50 +0900

    add Scala 3

--- src/test/scala/scalaprops/quickstart/ListTest.scala
+++ src/test/scala/scalaprops/quickstart/ListTest.scala
@@ -8,12 +8,12 @@ import scalaprops._
 object ListTest extends Scalaprops {
 
   val revAndRevIsOriginal =
-    Property.forAll { xs: List[Int] => xs.reverse.reverse == xs }
+    Property.forAll { (xs: List[Int]) => xs.reverse.reverse == xs }
 
   val revIsOriginal =
-    Property.forAll { xs: List[Int] => xs.reverse == xs }
+    Property.forAll { (xs: List[Int]) => xs.reverse == xs }
 
   val empty =
-    Property.forAll { xs: List[Int] => xs.isEmpty }
+    Property.forAll { (xs: List[Int]) => xs.isEmpty }
       .ignore("ignore a test")
 }
